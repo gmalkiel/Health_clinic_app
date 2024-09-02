cb308a6e7d768523190dedef165c027b564f0748
gmalkiel <galit2944@gmail.com>
2024-09-02 21:58:09 +0300

    2157

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-function sayHello(name){
+import express from "express"
+import { getAllTherapists, getTherapist, createTherapist } from "./database.js"
+
+const app = express()
+
+app.get("/therapists", async (req, res) => {
+    const therapists = await getAllTherapists()
+    res.send(therapists)
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
+
+
+/*function sayHello(name){
     console.log('hello ' + name);
 }
-sayHello('galit');
+sayHello('galit');*/
--- database.js
+++ database.js
@@ -10,7 +10,47 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE
 }).promise()
 
-/*const pool = mysql.createPool({
+
+export async function getAllTherapists() {
+    const [rows] = await pool.query("select * from Therapists")
+    return rows
+}
+  
+//const therapists = await getAllTherapists()
+//console.log(therapists)
+
+export async function getTherapist(id) {
+  const [rows] = await pool.query(`
+  SELECT * 
+  FROM Therapists
+  WHERE TherapistID = ?
+  `, [id])
+  return rows[0]
+}
+
+//const therapist = await getTherapist(4)
+//console.log(therapist)
+
+export async function createTherapist(Name, IDNumber, DateOfBirth, Email, Phone) {
+  const [result] = await pool.query(`
+  INSERT INTO Therapists (Name, IDNumber, DateOfBirth, Email, Phone)
+  VALUES (?, ?, ?, ?, ?)
+  `, [Name, IDNumber, DateOfBirth, Email, Phone])
+  const id = result.insertId
+  return getTherapist(id)
+}
+
+//const therapist = await createTherapist("Oren", "555555", "1992-09-06", "therapist8@example.com", "123-456-7893")
+//console.log(therapist)
+
+
+
+
+
+/*
+thing to save just in case
+
+const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: 'galit2944',
@@ -19,11 +59,3 @@ const pool = mysql.createPool({
 
 const result = await pool.query("select * from Therapists")
 console.log(result)*/
-
-async function getAllTherapists() {
-    const [rows] = await pool.query("select * from Therapists")
-    return rows
-}
-  
-const therapists = await getAllTherapists()
-console.log(therapists)
