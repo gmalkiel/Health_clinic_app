20bc2dfb676229c8303c2b16b6d9c36c0817db88
Moshayov <Moshayov@g.jct.ac.il>
2024-09-08 19:17:38 +0300

    client

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,50 +1,15 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../css/Login.css';      
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import bcrypt from 'bcryptjs';
+import CalendarComponent from '../components/schedule'
 
 const Home = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-
-
-
   return (
-    <div className='login-container'>
-      <div className="header">
-        <div className='title'>im in other page</div>
-        <div className="user-info">
-          <i className="fas fa-user"></i>
-          <span className="user_name">USER </span>
+   <>
+     <div>
+            <h1>לוח שנה לניהול פגישות</h1>
+            <CalendarComponent userType="admin" username="" />
         </div>
-      </div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit} className="login-form">
-        <div className="form-group">
-          <i className="fas fa-user"></i>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter your username"
-          />
-        </div>
-        <div className="form-group">
-          <i className="fas fa-lock"></i>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-          />
-        </div>
-        <button type="submit" className="login-button">Login</button>
-        {error && <p>{error}</p>}
-      </form>
-      <p>Dont have an account? <a href="/register">Register</a></p>
-    </div>
+   </>
   );
 };
 
