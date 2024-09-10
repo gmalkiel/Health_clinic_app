02c50f7e9785601b016f930d3e06fa6aba75f24c
SaraOshritYosian <saraoshr@g.jct.ac.il>
2024-09-10 18:17:37 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import AddTherapist from '../src/components/AddTherapist';
 //import DeleteTherapist from '../src/components/DeleteTherapist'; // Component to delete therapist
 import TherapistDetails from '../src/components/TherapistDetails';
 import AddPatient from '../src/components/AddPatient';
+import AddRestOfPatient from '../src/components/AddRestOfPatient';
 
 
 function App() {
@@ -26,6 +27,8 @@ function App() {
         <Route path="/patients/:TherapistID" element={<Patients />} />
         <Route path="/addTherapist" element={<AddTherapist />} />
         <Route path="/addPatient" element={<AddPatient />} />
+        <Route path="/addRestOfPatient/:PatientID" element={<AddRestOfPatient />} />
+
         {/*טיפול במצב מחיקה של מטפל מה לעשות עם מטופלים שקשורים אליו האם למחוק אותם או לשייך אותם למטפל אחר? 
         <Route path="/delete-therapist/:id" element={<DeleteTherapist />} />*/}
       </Routes>
