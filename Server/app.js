970b7cb21604f0d8de22e15038f0c13ca1c6bad3
Moshayov <Moshayov@g.jct.ac.il>
2024-09-12 21:40:55 +0300

    הוספת אפשרות יצירת סיכום דרך הלוז

--- Server/app.js
+++ Server/app.js
@@ -262,38 +262,6 @@ app.post("/manager", async (req, res) => {
     res.status(500).json({ error: 'Error adding manager' });
   }
 });
-/*
-app.post('/session/:PatientID', async (req, res) => {
-  const { PatientID } = req.params;
-  const { SessionContent, SessionSummary, ImagePath,CurrentDate } = req.body;
-
-  try {
-   
-
-    // Insert data into database
-    const newSession = await db.createNewSession(PatientID, CurrentDate, SessionContent, SessionSummary, ImagePath);
-    res.status(200).send(newSession);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error creating session');
-  }
-});
-*/
-/*
-app.post('/session/:PatientID', async (req, res) => {
-  const { PatientID } = req.params;
-  const { SessionContent, SessionSummary, ImagePath } = req.body;
-
-  try {
-    const currentDate = new Date();
-    const newSession = await db.createNewSession(PatientID, currentDate, SessionContent, SessionSummary, ImagePath);
-    res.status(200).send(newSession);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error creating session');
-  }
-});
-*/
 
 app.post('/addPatient', async (req, res) => {
   if (!req.body) {
