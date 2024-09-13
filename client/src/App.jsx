18d45c6c62104643e48dfdf7d5686826f98c2f0c
Moshayov <Moshayov@g.jct.ac.il>
2024-09-13 15:24:30 +0300

    1524

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Replace_Manager from './components/Replace_Manager';
 import Layout from '../src/components/Layout';
 import { UserProvider } from '../src/components/UserContext';
 import SessionDisplay from './components/SessionDisplay';
+import Messages from '../src/components/Messages';
 function App() {
   return (
     <UserProvider> 
@@ -38,6 +39,7 @@ function App() {
         <Route path="/addRestOfPatient/:PatientID" element={<AddRestOfPatient />} />
         <Route path="/navigation" element={<Navigation />} />
         <Route path="/sessions/:SessionID" element={<SessionDisplay />} />
+        <Route path="/messages/:therapistID" element={<Messages />} />
 
         {/*טיפול במצב מחיקה של מטפל מה לעשות עם מטופלים שקשורים אליו האם למחוק אותם או לשייך אותם למטפל אחר? 
         <Route path="/delete-therapist/:id" element={<DeleteTherapist />} />*/}
