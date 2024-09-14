567d7a7d980966aeb617f6b90fffaa58e2308ec2
gmalkiel <galit2944@gmail.com>
2024-09-14 22:48:09 +0300

    2248

--- Server/controllers/sessionController.js
+++ Server/controllers/sessionController.js
@@ -26,17 +26,66 @@ export const getSessionsByPatient = async (req, res) => {
     }
 };
 
-export const createSession = async (req, res) => {
-    const { PatientID } = req.params;
-    const { SessionContent, SessionSummary, ImagePath, CurrentDate } = req.body;
+export async function createNewSession(PatientID, SessionDate, SessionContent, SessionSummary, ArtworkImagePath) {
+    const connection = await pool.getConnection();
+    await connection.beginTransaction(); // התחלת טרנזקציה
+  
     try {
-        const newSession = await db.createSession(PatientID, CurrentDate, SessionContent, SessionSummary, ImagePath);
-        res.status(200).send(newSession);
+      const imagePath = path.resolve(ArtworkImagePath); 
+      const imageData = fs.readFileSync(imagePath);
+      const sessionId = await createSession(connection, {
+        SessionContent,
+        SessionSummary,
+        PatientID,
+        ArtworkImage: imageData,
+        SessionDate
+      });
+  
+      const [therapistRow] = await connection.query(`
+        SELECT TherapistID FROM TherapistPatients WHERE PatientID = ?
+      `, [PatientID]);
+  
+      if (therapistRow.length === 0) {
+        throw new Error('לא נמצא מטפל עבור המטופל');
+      }
+  
+      const therapistID = therapistRow[0].TherapistID;
+  
+      const [therapist] = await connection.query(`
+        SELECT SessionPrice FROM Therapists WHERE TherapistID = ?
+      `, [therapistID]);
+  
+      const sessionPrice = therapist[0].SessionPrice;
+  
+      await connection.query(`
+        UPDATE Patients
+        SET remainingSessions = remainingSessions - 1, remainingPayment = remainingPayment + ?
+        WHERE PatientID = ?
+      `, [sessionPrice, PatientID]);
+  
+      const [patient] = await connection.query(`
+        SELECT Name, remainingPayment FROM Patients WHERE PatientID = ?
+      `, [PatientID]);
+  
+      if (patient[0].remainingPayment > 0) {
+        const paymentAlertMessage = `התראת תשלום למטופל ${patient[0].Name} (ID: ${PatientID}) - יתרת תשלום: ${patient[0].remainingPayment} ש"ח.`;
+        
+        await connection.query(`
+          INSERT INTO Messages (TherapistID, Content) VALUES (?, ?)
+        `, [therapistID, paymentAlertMessage]);
+      }
+  
+      await connection.commit();
+      return getSession(sessionId); 
+  
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error creating session');
+      await connection.rollback();
+      console.error('שגיאה במהלך יצירת הסשן:', error.message);
+      throw error; 
+    } finally {
+      connection.release();
     }
-};
+  }
 
 export const updateSession = async (req, res) => {
     const { id } = req.params;
